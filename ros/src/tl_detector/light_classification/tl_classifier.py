cb857111f7e458340edcfe18b3cd275c0d0ccc0b
KKosukeee <kousuke.newlife@gmail.com>
2019-05-02 22:24:44 +0900

    Created helper methods!!!

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,106 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import cv2 as cv
+import numpy as np
+from ros.src.tl_detector.utils import label_map_util
+from ros.src.tl_detector.utils import visualization_utils as vis_util
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+
+        # TODO: Modify variables below
+        PATH_TO_PB_FILE = ''
+        PATH_TO_LABELS = ''
+        self.NUM_CLASSES = 4
+        self.THRESHOLD = 0.5
+
+        self.graph = tf.Graph()
+
+        # Detection model loaded
+        with self.graph.as_default():
+            graph_def = tf.GraphDef()
+
+            # Parsing computational graph from pb file
+            with tf.gfile.GFile(PATH_TO_PB_FILE, 'rb') as fid:
+                serialized = fid.read()
+                graph_def.ParseFromString(serialized)
+                tf.import_graph_def(graph_def, name='')
+
+            # Those are the placeholder for the detection
+            self.input_img = self.graph.get_tensor_by_name('image_tensor:0')
+            self.res_bbox = self.graph.get_tensor_by_name('detection_boxes:0')
+            self.res_score = self.graph.get_tensor_by_name('detection_scores:0')
+            self.res_classes = self.graph.get_tensor_by_name('detection_classes:0')
+            self.num_detection = self.graph.get_tensor_by_name('num_detections:0')
+
+        # Load pbtxt file into memory
+        self.label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+        categories = label_map_util.convert_label_map_to_categories(
+            self.label_map, max_num_classes=self.NUM_CLASSES, use_display_name=False
+        )
+        self.category_index = label_map_util.create_category_index(categories)
+
+    def inference_for_single_image(self, image):
+        """
+        does the inference and returns the result.
+        Args:
+            image: 3D array where the shape is [height, width, 3]
+
+        Returns:
+            tuple<np.ndarray>: tuple of np.ndarrays where first element is the detected boxes,
+                and the second element is detected classes, and the last one is the detected
+                confidences. \
+                positive_boxes is the 2D array where the shape is [N, 4] and for each
+                element has the following information: [ymin, xmin, ymax, xmax]. Keep note they
+                are in normalized format and the range is [0, 1]
+
+                positive_classes is 2D array with the shape of [N, 1]. ith detected box's class
+                can be found by positive_classes[i][0]
+
+                positive_confidences is 2D array with the shape of [N, 1]. ith detected box's
+                confidence can be found by positive_confidences[i][0] which is in range of [0, 1.0]
+
+        """
+        # Start session for a prediction
+        with tf.Session(graph=self.graph) as sess:
+            height, width = image.shape[:2]
+
+            # Run the detector!!!
+            (boxes, scores, classes, num) = sess.run(
+                [self.res_bbox, self.res_score, self.res_classes, self.num_detection],
+                feed_dict={self.input_img: image[None, ...]}
+            )
+
+            # Get detections with the confidence >= self.THRESHOLD
+            positive_indices = np.where(scores[0] >= self.THRESHOLD)[0]
+            positive_boxes = boxes[0][positive_indices]
+            positive_classes = classes[0][positive_indices]
+            positive_confidences = scores[0][positive_indices]
+
+        return (positive_boxes, positive_classes, positive_confidences)
+
+    def visualize_result(self, image, boxes, classes, confidences):
+        """
+        Visualize bounding boxes on the image in place
+        Args:
+            image: 3D array where the shape is [height, width, 3]
+            boxes: 2D array which is returned by inference_for_single_image method
+            classes: 2D array which is returned by inference_for_single_image method
+            confidences: 2D array which is returned by inference_for_single_image method
+
+        Returns:
+
+        """
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(confidences),
+            self.category_index,
+            use_normalized_coordinates=True,
+            min_score_thresh=0.5,
+            line_thickness=3)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
