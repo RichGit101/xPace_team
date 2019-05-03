f57f368b86633e00183263a299ae7b2af1d70ddf
siqb <siqb00@gmail.com>
2019-05-02 17:50:58 -0700

    Fixing typo in twist_controller.py introduced by first commit. Changed "wheel_radium" -> "wheel_radius."

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -65,6 +65,6 @@ class Controller(object):
 	elif throttle < 0.1 and vel_error < 0:
 	    throttle = 0
 	    decel = max(vel_error, self.decel_limit)
-	    brake = self.vehicle_mass * self.wheel_radium *abs(decel) # Torque - N*m
+	    brake = self.vehicle_mass * self.wheel_radius *abs(decel) # Torque - N*m
 
 	return throttle, brake, steer
